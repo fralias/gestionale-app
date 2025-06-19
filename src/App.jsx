750ec5edc0803e1ff0fddf31c96fc98592b4a92b
fralias <fralias@gmail.com>
2025-06-19 20:04:52 +0200

    Configurazione iniziale del progetto
    Configurazione iniziale del progetto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold', color: '#10B981' }}>
+        Progetto Caricato Correttamente!
+      </h1>
+      <p style={{ marginTop: '1rem', color: '#4B5563' }}>
+        Il server di sviluppo è attivo. Ora possiamo procedere a ricostruire l'interfaccia.
+      </p>
+    </div>
+  );
+}
+
+export default App;
