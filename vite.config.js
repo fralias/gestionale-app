d00b73c00cab74a2573f2c6de58dd7d85184d7d4
fralias <fralias@gmail.com>
2025-06-21 13:38:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src', // Aggiungi questa riga
+  build: {
+    outDir: '../dist'
+  }
 })
